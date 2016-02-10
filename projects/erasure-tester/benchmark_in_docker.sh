e3dbb1733d8213dcc095bcea7b1682042a9f668f
Sébastien Vaucher <278609+sebva@users.noreply.github.com>
2016-02-10 08:56:03 +0100

    Adapt for Docker-compose 1.6

--- projects/erasure-tester/benchmark_in_docker.sh
+++ projects/erasure-tester/benchmark_in_docker.sh
@@ -5,8 +5,9 @@
 mkdir -p results
 
 # Clean everything at Ctrl+C
-trap 'docker-compose --x-networking stop && yes y | docker-compose --x-networking rm && exit' TERM INT
+trap 'docker-compose down && exit' TERM INT
 
-docker-compose --x-networking scale redis-master=5 redis-standalone=1 erasure=0 benchmark=1 # For 5 Redis servers
-docker-compose --x-networking logs benchmark
-docker-compose --x-networking stop && yes y | docker-compose --x-networking rm
+docker-compose up -d redis-standalone
+docker-compose scale redis-master=5 redis-standalone=1 erasure=0 benchmark=1 # For 5 Redis servers
+docker-compose logs benchmark
+docker-compose down
