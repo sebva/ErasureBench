932c96501773b1c86dd1d82e85461cc1ac887c63
Sébastien Vaucher <278609+sebva@users.noreply.github.com>
2016-03-11 10:24:14 +0100

    Create a separate Python file for Redis stuff

--- projects/erasure-tester/benchmark_in_docker.sh
+++ projects/erasure-tester/benchmark_in_docker.sh
@@ -8,6 +8,6 @@ mkdir -p results
 trap 'docker-compose down && exit' TERM INT
 
 docker-compose up -d redis-standalone
-docker-compose scale redis-master=5 redis-standalone=1 erasure=0 benchmark=1 # For 5 Redis servers
+docker-compose scale redis-master=0 redis-standalone=0 erasure=0 benchmark=1 # Redis nodes are started from the script
 docker-compose logs benchmark
 docker-compose down
--- projects/erasure-tester/container-scripts/benchmark.py
+++ projects/erasure-tester/container-scripts/benchmark.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import json
+import logging
 import os
 import re
 import socket
@@ -9,6 +10,7 @@ from datetime import datetime
 import signal
 from benchmarks_impl import BenchmarksImpl
 from plot_results import main as output_plots
+from redis_cluster import RedisCluster
 from time import sleep
 
 
@@ -19,10 +21,10 @@ class Benchmarks:
         self.java = java
 
         # 2 is forbidden due to Redis limitation on Cluster size
-        self.redis_size = [5, 4, 3, 1, 0]
-        self.erasure_codes = ['Null', 'ReedSolomon', 'SimpleRegenerating']
+        self.redis_size = [5, 1, 0]
+        self.erasure_codes = ['Null']
         self.stripe_sizes = [10]
-        self.parity_sizes = [2, 4, 6]
+        self.parity_sizes = [2]
         self.src_sizes = [5]
         self.first = True
         self.results = []
@@ -33,20 +35,23 @@ class Benchmarks:
 
     def run_benchmarks(self):
         for rs in self.redis_size:
-            self.scale_redis(rs)
             for ec in self.erasure_codes:
                 for ss in self.stripe_sizes:
                     for ps in self.parity_sizes:
                         for src in self.src_sizes:
-                            sb = 'Jedis' if rs > 0 else 'Memory'
-                            config = [ec, rs, sb, ss, ps, src]
-                            print("Running with " + str(config))
-                            self.restart(*config)
                             for b in self.benches:
-                                self._run_benchmark(b, config)
+                                with RedisCluster(rs) as redis:
+                                    sb = 'Jedis' if rs > 0 else 'Memory'
+                                    config = [ec, rs, sb, ss, ps, src]
+                                    print("Running with " + str(config))
+                                    self.restart(redis, *config)
+
+                                    try:
+                                        self._run_benchmark(b, config)
+                                    except Exception as ex:
+                                        logging.exception("The benchmark crashed, continuing with the rest...")
 
     def _run_benchmark(self, bench, config):
-        self.redis_flushall(config[1])
         bench_name = bench.__name__
         print("    " + bench_name)
         self.results.append({
@@ -59,104 +64,7 @@ class Benchmarks:
         with open(self.log_file_base + '.json', 'w') as out:
             json.dump(self.results, out, indent=4)
 
-    def scale_redis(self, cluster_size, brutal=False):
-        if cluster_size <= 1:
-            return True
-
-        print("Scaling Redis to %d nodes" % cluster_size)
-        nodes = self.get_redis_masters()
-        if len(nodes) < cluster_size:
-            self.add_new_redis_masters(cluster_size, nodes)
-        else:
-            while len(nodes) > cluster_size:
-                if brutal:
-                    self.kill_a_redis_node(nodes)
-                else:
-                    self.remove_a_redis_node(nodes)
-
-        return True
-
-    def add_new_redis_masters(self, cluster_size, old_nodes):
-        subprocess.check_call(['docker-compose', 'scale', 'redis-master=%d' % cluster_size])
-        nb_new_nodes = cluster_size - len(old_nodes)
-
-        new_ips = [':'.join(map(str, x)) for x in get_redis_nodes()[-nb_new_nodes:]]
-        print(new_ips)
-        master_ip_port = old_nodes[0]['ip_port']
-
-        for ip in new_ips:
-            sleep(1)
-            subprocess.check_call(['ruby', 'redis-trib.rb', 'add-node', ip, master_ip_port])
-
-        sleep(2)
-        new_nodes = [x for x in self.get_redis_masters() if x['ip_port'] in new_ips]
-        shards_to_move_per_node = round(16384 / cluster_size / len(old_nodes))
-
-        for new_node in new_nodes:
-            for old_node in old_nodes:
-                sleep(0.5)
-                fix = subprocess.Popen(['ruby', 'redis-trib.rb', 'fix', master_ip_port], stdin=subprocess.PIPE, stdout=subprocess.DEVNULL)
-                fix.communicate(b'yes\n')
-                fix.wait()
-                self.transfer_slots(old_node['id'], new_node['id'], shards_to_move_per_node, master_ip_port)
-
-        subprocess.check_call(['ruby', 'redis-trib.rb', 'info', master_ip_port])
-
-    def get_redis_masters(self):
-        nodes = [x.split(' ') for x in
-                 subprocess.check_output('redis-cli -h erasuretester_redis-master_1 CLUSTER NODES'.split(' ')).decode(
-                         'UTF-8').splitlines()]
-        return [{
-                    'id': x[0],
-                    'ip_port': x[1],
-                    'is_number_1': 'myself' in x[2]
-                } for x in nodes]
-
-    def elect_redis_victim(self, nodes):
-        return nodes[-1]
-
-    def redis_flushall(self, rs):
-        if rs == 1:
-            subprocess.check_call(['redis-cli', '-h', 'erasuretester_redis-standalone_1', 'FLUSHALL'])
-        elif rs >= 2:
-            nodes = self.get_redis_masters()
-            for node in nodes:
-                subprocess.check_call(['redis-cli', '-h', node['ip_port'].split(':')[0], 'FLUSHALL'])
-
-    def kill_a_redis_node(self, nodes):
-        victim = self.elect_redis_victim(nodes)
-
-        # Brutally kill the node
-        subprocess.check_call(['redis-cli', '-h', victim['ip_port'].split(':')[0], 'SHUTDOWN'])
-        for node in nodes:
-            subprocess.check_call(['redis-cli', '-h', node['ip_port'].split(':')[0], 'CLUSTER', 'FORGET', victim['id']])
-
-        redistrib = subprocess.Popen(['ruby', 'redis-trib.rb', 'fix', nodes[0]['ip_port']], stdin=subprocess.PIPE, stdout=subprocess.DEVNULL)
-        redistrib.communicate(b'yes\n')
-        redistrib.wait()
-
-    def remove_a_redis_node(self, nodes):
-        victim = self.elect_redis_victim(nodes)
-        master_ip_port = [x['ip_port'] for x in nodes if x['is_number_1']][0]
-        info = subprocess.check_output(['ruby', 'redis-trib.rb', 'info', master_ip_port]).decode('UTF-8').splitlines()
-        slots_to_remove = int(
-                re.search(r'([0-9]+) slots', [x for x in info if x.startswith(victim['ip_port'])][0]).group(1))
-        slots_to_remove_per_node = int(slots_to_remove / len(nodes))
-        slots_to_remove -= slots_to_remove_per_node * len(nodes)
-
-        for node in nodes:
-            self.transfer_slots(victim['id'], node['id'], slots_to_remove_per_node, master_ip_port)
-        if slots_to_remove > 0:
-            self.transfer_slots(victim['id'], nodes[0]['id'], slots_to_remove, master_ip_port)
-
-        subprocess.check_call(['ruby', 'redis-trib.rb', 'del-node', master_ip_port, victim['id']])
-
-    def transfer_slots(self, from_id, to_id, amount, master_ip_port):
-        print('Transfering %d slots...' % amount)
-        subprocess.check_call(('ruby redis-trib.rb reshard --from %s --to %s --slots %d --yes %s' % (
-            from_id, to_id, amount, master_ip_port)).split(' '), stdout=subprocess.DEVNULL)
-
-    def restart(self, erasure, redis_size, storage, stripe=None, parity=None, src=None, quiet=True):
+    def restart(self, redis, erasure, redis_size, storage, stripe=None, parity=None, src=None, quiet=True):
         if self.first:
             self.first = False
         else:
@@ -178,7 +86,7 @@ class Benchmarks:
         if redis_size > 1:
             params += ['--redis-cluster']
 
-        env = {'REDIS_ADDRESS': get_redis_node_str(redis_size)}
+        env = {'REDIS_ADDRESS': redis.get_master_node_str(redis_size)}
         self.java.start(params, env)
 
 
@@ -213,38 +121,8 @@ def kill_pid(proc):
         proc.wait()
 
 
-def start_redis_cluster():
-    args = ['ruby', 'redis-trib.rb', 'create']
-
-    args += [':'.join(map(str, x)) for x in get_redis_nodes()]
-
-    redistrib = subprocess.Popen(args, stdin=subprocess.PIPE)
-    redistrib.communicate(b'yes\n')
-    redistrib.wait()
-
-
-def get_redis_nodes():
-    redis_nodes = []
-    try:
-        i = 1
-        while True:
-            redis_nodes.append(socket.getaddrinfo('erasuretester_redis-master_%d' % i, 6379, socket.AF_INET)[0][4])
-            i += 1
-    except socket.gaierror:
-        pass
-    return redis_nodes
-
-
-def get_redis_node_str(redis_size):
-    container_name = 'standalone' if redis_size <= 1 else 'master'
-    return ':'.join(
-            map(str, socket.getaddrinfo('erasuretester_redis-%s_1' % container_name, 6379, socket.AF_INET)[0][4]))
-
-
 if __name__ == '__main__':
     sleep(10)
-    print("Configuring Redis cluster")
-    start_redis_cluster()
     print("Python client ready, starting benchmarks")
     benchmarks = Benchmarks(JavaProgram())
     benchmarks.run_benchmarks()
