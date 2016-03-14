218a4e852d6a419258f22ef4eda3eece8be50e32
Sébastien Vaucher <278609+sebva@users.noreply.github.com>
2016-03-14 16:18:05 +0100

    Fix Docker containers cleanup in benchmark_on_cluster script

--- projects/erasure-tester/benchmark_on_cluster.sh
+++ projects/erasure-tester/benchmark_on_cluster.sh
@@ -20,7 +20,7 @@ docker push localhost:5000/redis-master:latest
 
 kill ${ssh_pid}
 
-sshpass -p "${password}" ssh debian@${MANAGER_IP} 'docker -H tcp://0.0.0.0:5732 stop $(docker ps -q) && docker -H tcp://0.0.0.0:5732 rm $(docker ps -aq)'
+sshpass -p "${password}" ssh debian@${MANAGER_IP} 'docker -H tcp://0.0.0.0:5732 stop $(docker -H tcp://0.0.0.0:5732 ps -q) && docker -H tcp://0.0.0.0:5732 rm $(docker -H tcp://0.0.0.0:5732 ps -aq)'
 
 sshpass -p "${password}" rsync -av --copy-links cluster/ debian@${MANAGER_IP}:~/erasuretester
 sshpass -p "${password}" ssh debian@${MANAGER_IP} 'cd ~/erasuretester && exec ./run_benchmarks.sh'
