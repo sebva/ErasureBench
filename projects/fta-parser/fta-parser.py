58f3c5a2a149ea7de292b886a42fd71c3c4fe979
Sébastien Vaucher <278609+sebva@users.noreply.github.com>
2016-03-16 16:52:42 +0100

    Change the FTA parser to use SQLite databases
    We need to do one join, which would be tricky to do using CSV files.
    For the moment, the code works as before but is a giant resource hog.

--- projects/fta-parser/fta-parser.py
+++ projects/fta-parser/fta-parser.py
@@ -1,47 +1,49 @@
 #!/usr/bin/env python3
 import os
+import random
 
 import matplotlib.pyplot as plt
+import sqlite3
 
 
-def parse_trace(set_name, filename):
+def parse_trace(set_name, db_file):
     print(set_name)
     try:
-        with open(filename) as file:
-            all_events = list()
-            for line in [x for x in file.readlines() if not x.startswith('#')]:
-                splitted = line.split("\t")
-                machine_name = splitted[4]
-                is_up = splitted[5] == '1'
-                time_start = float(splitted[6])
-                time_end = float(splitted[7])
-                all_events.append((machine_name, is_up, time_start, time_end))
+        sql = sqlite3.connect(db_file)
+        #sql.row_factory = sqlite3.Row
 
-            count = len({x[0] for x in all_events})
-            events = [(x[2], False) for x in all_events if not x[1]] + [(x[3], True) for x in all_events if not x[1]]
-            events.sort(key=lambda x: x[0])
-            print(count)
-            x_axis = list()
-            y_axis = list()
+        cur = sql.cursor()
+        cur.execute(r'SELECT event_start_time, event_type FROM event_trace JOIN component ON event_trace.component_id = component.component_id WHERE (event_type = 0 OR event_type = 1) AND component_type = 0 ORDER BY event_start_time;')
 
-            all_events.sort(key=lambda e: e[2])
-            for event in events:
-                x_axis.append(event[0])
-                if event[1]:
-                    count += 1
-                else:
-                    count -= 1
-                y_axis.append(count)
+        count = 0
 
-            plt.plot(x_axis, y_axis)
-            plt.show()
+        x_axis = list()
+        y_axis = list()
+
+        event = cur.fetchone()
+        while event is not None:
+            x_axis.append(event[0])
+            if event[1] == 1:
+                count += 1
+            else:
+                count -= 1
+            y_axis.append(count)
+            event = cur.fetchone()
+
+        count += cur.rowcount
+
+        cur.close()
+        sql.close()
+
+        plt.plot(x_axis, y_axis)
+        plt.show()
 
     except ValueError:
         pass
 
 
 if __name__ == '__main__':
-    sets_names = [x for x in os.listdir('traces') if os.path.isdir(os.path.join('traces', x))]
-    files = [os.path.join('traces', x, 'event_trace.tab') for x in sets_names]
-    for (set_name, filename) in zip(sets_names, files):
-        parse_trace(set_name, filename)
+    databases = [x for x in os.listdir('databases') if x.endswith('.db')]
+    random.shuffle(databases)
+    for db in databases:
+        parse_trace(db[:-3], 'databases/' + db)
