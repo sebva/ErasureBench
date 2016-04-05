55235f5edd7facc9f81adc8d0acef533b0da702b
Sébastien Vaucher <278609+sebva@users.noreply.github.com>
2016-04-05 15:19:43 +0200

    Modify plot_result.py to account for multiple archives

--- projects/erasure-tester/plot_result.py
+++ projects/erasure-tester/plot_result.py
@@ -7,17 +7,18 @@ def plot_results(filename):
     with open(filename) as fp:
         results = json.load(fp)
 
-    results = [x for x in results if x['bench'] == 'bench_apache' and x['config'][1] == 60]
+    results = [x for x in results if x['bench'] in ('bench_bc', 'bench_10bytes', 'bench_apache')]
     for result in results:
         sort = sorted(result['results'].values(), key=lambda x: x['RS'], reverse=True)
         x_axis = [x['RS0'] - x['RS'] for x in sort]
-        y_axis = [x['OK Files'] - 1 for x in sort]
+        y_axis = [(x['OK Files'] / sort[0]['OK Files']) for x in sort]
         print(x_axis)
         print(y_axis)
 
-        plt.plot(x_axis, y_axis, label=result['config'][0])
+        plt.plot(x_axis, y_axis, label=result['bench'] + ' / ' + result['config'][0])
+        plt.title("Checksum benchmark, %d nodes" % result['config'][1])
 
-    plt.axis([0, 60, 0, 2516])
+    plt.axis(ymin=0, xmin=0)
     plt.xlabel("Dead nodes")
     plt.ylabel("Successful queries")
     plt.legend()
