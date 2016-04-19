075018250a6fc410be79daa9c15bc63c06df0689
Sébastien Vaucher <278609+sebva@users.noreply.github.com>
2016-04-19 10:26:27 +0200

    Parse overnet

--- projects/fta-parser/fta-parser.py
+++ projects/fta-parser/fta-parser.py
@@ -43,6 +43,41 @@ def parse_trace_method1(db_file, epoch=719163):
     return x_axis_np, y_axis_np
 
 
+def parse_trace_method2(db_file, epoch=719163):
+    print(db_file)
+    sql = sqlite3.connect(db_file)
+
+    cur = sql.cursor()
+
+    cur.execute(r'SELECT event_start_time, event_type FROM event_trace ORDER BY event_start_time;')
+
+    print("End query")
+
+    x_axis = list()
+    y_axis = list()
+
+    count = 0
+    events = cur.fetchall()
+    for event in events:
+        x_axis.append(event[0])
+        y_axis.append(count)
+
+        if event[1] == 0:
+            count += 1
+        elif event[1] == 1:
+            count -= 1
+
+        x_axis.append(event[0])
+        y_axis.append(count)
+
+    cur.close()
+    sql.close()
+
+    x_axis_np = np.array([x / 86400. + epoch for x in x_axis])
+    y_axis_np = np.array(y_axis)
+    return x_axis_np, y_axis_np
+
+
 def plot_trace(pdf, x_axis_np, y_axis_np, set_name):
     # Any filter available in scipy.signal can be applied
     # y_axis_np = signal.medfilt(y_axis_np, 51)
@@ -73,7 +108,7 @@ if __name__ == '__main__':
         plot_trace(pdf, *parse_trace_method1('databases/notre_dame07_cpu1.db'), set_name='nd_cpu')
         plot_trace(pdf, *parse_trace_method1('databases/notre_dame07_rood.db'), set_name='nd_rood')
         plot_trace(pdf, *parse_trace_method1('databases/oneping.db'), set_name='oneping')
-        plot_trace(pdf, *parse_trace_method1('databases/overnet03.db'), set_name='overnet')
+        plot_trace(pdf, *parse_trace_method2('databases/overnet03.db'), set_name='overnet')
         plot_trace(pdf, *parse_trace_method1('databases/pnnl07.db'), set_name='pnnl')
         plot_trace(pdf, *parse_trace_method1('databases/sdsc.db'), set_name='sdsc')
         plot_trace(pdf, *parse_trace_method1('databases/teragrid2.db'), set_name='teragrid')
