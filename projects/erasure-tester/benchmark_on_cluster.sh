59f41dff0e73feb2318e148e2307e705b7fda0ab
Sébastien Vaucher <278609+sebva@users.noreply.github.com>
2016-05-03 12:48:15 +0200

    Adapt to arch cluster

--- projects/erasure-tester/benchmark_on_cluster.sh
+++ projects/erasure-tester/benchmark_on_cluster.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-MANAGER_IP=172.16.0.167
+MANAGER_IP=172.16.2.79
 
 echo Password:
 read -s password
@@ -8,7 +8,7 @@ read -s password
 ./gradlew --daemon docker
 
 # Not very secure way of passing the password...
-sshpass -p "${password}" ssh -N -L 5000:localhost:5000 debian@${MANAGER_IP} &
+sshpass -p "${password}" ssh -N -L 5000:localhost:5000 arch@${MANAGER_IP} &
 ssh_pid=$!
 
 sleep 5
@@ -20,7 +20,7 @@ docker push localhost:5000/redis-master:latest
 
 kill ${ssh_pid}
 
-sshpass -p "${password}" ssh debian@${MANAGER_IP} 'docker -H tcp://0.0.0.0:5732 stop $(docker -H tcp://0.0.0.0:5732 ps -q) && docker -H tcp://0.0.0.0:5732 rm $(docker -H tcp://0.0.0.0:5732 ps -aq)'
+sshpass -p "${password}" ssh arch@${MANAGER_IP} 'docker -H tcp://0.0.0.0:5732 stop $(docker -H tcp://0.0.0.0:5732 ps -q) && docker -H tcp://0.0.0.0:5732 rm $(docker -H tcp://0.0.0.0:5732 ps -aq)'
 
-sshpass -p "${password}" rsync -av --copy-links cluster/ debian@${MANAGER_IP}:~/erasuretester
-sshpass -p "${password}" ssh debian@${MANAGER_IP} 'cd ~/erasuretester && exec ./run_benchmarks.sh'
+sshpass -p "${password}" rsync -av --copy-links cluster/ arch@${MANAGER_IP}:~/erasuretester
+#sshpass -p "${password}" ssh arch@${MANAGER_IP} 'cd ~/erasuretester && exec ./run_benchmarks.sh'
