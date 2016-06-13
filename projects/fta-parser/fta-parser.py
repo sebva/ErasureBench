1742f0d4cc830e6b9c00bc8815ee4a08919e34a3
Sébastien Vaucher <278609+sebva@users.noreply.github.com>
2016-06-13 11:59:52 +0200

    Display the websites trace nicely in the report

--- projects/fta-parser/fta-parser.py
+++ projects/fta-parser/fta-parser.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sqlite3
+from datetime import timezone, tzinfo, datetime
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -254,49 +255,83 @@ def simulate(db_file):
     cur.close()
     sql.close()
 
-if __name__ == '__main__':
-    simulate('databases/oneping.db')
 
-    with PdfPages('fta.pdf') as pdf:
-        plot_trace(pdf, *parse_trace_method1('databases/cae.db'), set_name='cae')
-        bar(pdf, *compute_histogram1('databases/cae.db'), set_name='cae')
-        plot_trace(pdf, *parse_trace_method1('databases/cs.db'), set_name='cs')
-        bar(pdf, *compute_histogram1('databases/cs.db'), set_name='cs')
-        plot_trace(pdf, *parse_trace_method1('databases/dath14.db'), set_name='dath14')
-        #bar(pdf, *compute_histogram1('databases/dath14.db'), set_name='dath14')
-        plot_trace(pdf, *parse_trace_method1('databases/deug.db'), set_name='deug')
-        bar(pdf, *compute_histogram1('databases/deug.db'), set_name='deug')
-        plot_trace(pdf, *parse_trace_method1('databases/g5k06.db'), set_name='g5k06')
-        #bar(pdf, *compute_histogram1('databases/g5k06.db'), set_name='g5k06')
-        plot_trace(pdf, *parse_trace_method1('databases/glow.db'), set_name='glow')
-        bar(pdf, *compute_histogram1('databases/glow.db'), set_name='glow')
-        plot_trace(pdf, *parse_trace_method1('databases/lanl05_union.db'), set_name='lanl05')
-        bar(pdf, *compute_histogram1('databases/lanl05_union.db'), set_name='lanl05')
-        plot_trace(pdf, *parse_trace_method1('databases/ldns04.db'), set_name='ldns')
-        #bar(pdf, *compute_histogram1('databases/ldns04.db'), set_name='ldns')
-        plot_trace(pdf, *parse_trace_method1('databases/lri.db'), set_name='lri')
-        bar(pdf, *compute_histogram1('databases/lri.db'), set_name='lri')
-        plot_trace(pdf, *parse_trace_method1('databases/microsoft99.db'), set_name='microsoft')
-        #bar(pdf, *compute_histogram1('databases/microsoft99.db'), set_name='microsoft')
-        plot_trace(pdf, *parse_trace_method1('databases/notre_dame07_cpu1.db'), set_name='nd_cpu')
-        #bar(pdf, *compute_histogram1('databases/notre_dame07_cpu1.db'), set_name='nd_cpu')
-        plot_trace(pdf, *parse_trace_method1('databases/notre_dame07_rood.db'), set_name='nd_rood')
-        #bar(pdf, *compute_histogram1('databases/notre_dame07_rood.db'), set_name='nd_rood')
-        plot_trace(pdf, *parse_trace_method3('databases/oneping.db'), set_name='oneping')
-        bar(pdf, *compute_histogram3('databases/oneping.db'), set_name='oneping')
-        plot_trace(pdf, *parse_trace_method2('databases/overnet03.db'), set_name='overnet')
-        bar(pdf, *compute_histogram2('databases/overnet03.db'), set_name='overnet')
-        plot_trace(pdf, *parse_trace_method1('databases/pnnl07.db'), set_name='pnnl')
-        #bar(pdf, *compute_histogram1('databases/pnnl07.db'), set_name='pnnl')
-        plot_trace(pdf, *parse_trace_method1('databases/sdsc.db'), set_name='sdsc')
-        bar(pdf, *compute_histogram1('databases/sdsc.db'), set_name='sdsc')
-        plot_trace(pdf, *parse_trace_method1('databases/teragrid2.db'), set_name='teragrid')
-        bar(pdf, *compute_histogram1('databases/teragrid2.db'), set_name='teragrid')
-        plot_trace(pdf, *parse_trace_method1('databases/ucb.db'), set_name='ucb')
-        bar(pdf, *compute_histogram1('databases/ucb.db'), set_name='ucb')
-        # In websites_02, the 0 epoch = 26/09/2001 16:11:10
-        # In pyplot, the 0 epoch = 01/01/0001 -1
-        #                Days    +1   16:11:10
-        websites_epoch = 730753 + 1 + 86400. / (16 * 3600 + 11 * 60 + 10)
-        plot_trace(pdf, *parse_trace_method1('databases/websites02.db', epoch=websites_epoch), set_name='websites')
-        bar(pdf, *compute_histogram1('databases/websites02.db'), set_name='websites')
+def pgfplotsfile(db_file, epoch_delta=0, start_time=0, end_time=10000000000, show_every=1):
+    sql = sqlite3.connect(db_file)
+
+    cur = sql.cursor()
+    cur.execute(r'SELECT event_start_time, event_type FROM event_trace WHERE event_start_time > ? AND event_start_time < ? ORDER BY event_start_time;',
+                (start_time, end_time))
+
+    print("date,size")
+
+    size = 0
+    count = 0
+    last_date_str = ""
+    events = cur.fetchall()
+    for event in events:
+        if event[1] == 1:
+            size += 1
+        elif event[1] == 0:
+            size -= 1
+
+        date = datetime.fromtimestamp(event[0] + epoch_delta, timezone.utc)
+        date_str = date.strftime('%Y-%m-%d %H:%M')
+        if date_str != last_date_str:
+            last_date_str = date_str
+            if count % show_every == 0:
+                print('%s,%d' % (date_str, size))
+            count += 1
+
+    cur.close()
+    sql.close()
+
+
+if __name__ == '__main__':
+    pgfplotsfile('databases/websites02.db', epoch_delta=1001779870, show_every=10)
+
+    # simulate('databases/oneping.db')
+    #
+    # with PdfPages('fta.pdf') as pdf:
+    #     plot_trace(pdf, *parse_trace_method1('databases/cae.db'), set_name='cae')
+    #     bar(pdf, *compute_histogram1('databases/cae.db'), set_name='cae')
+    #     plot_trace(pdf, *parse_trace_method1('databases/cs.db'), set_name='cs')
+    #     bar(pdf, *compute_histogram1('databases/cs.db'), set_name='cs')
+    #     plot_trace(pdf, *parse_trace_method1('databases/dath14.db'), set_name='dath14')
+    #     #bar(pdf, *compute_histogram1('databases/dath14.db'), set_name='dath14')
+    #     plot_trace(pdf, *parse_trace_method1('databases/deug.db'), set_name='deug')
+    #     bar(pdf, *compute_histogram1('databases/deug.db'), set_name='deug')
+    #     plot_trace(pdf, *parse_trace_method1('databases/g5k06.db'), set_name='g5k06')
+    #     #bar(pdf, *compute_histogram1('databases/g5k06.db'), set_name='g5k06')
+    #     plot_trace(pdf, *parse_trace_method1('databases/glow.db'), set_name='glow')
+    #     bar(pdf, *compute_histogram1('databases/glow.db'), set_name='glow')
+    #     plot_trace(pdf, *parse_trace_method1('databases/lanl05_union.db'), set_name='lanl05')
+    #     bar(pdf, *compute_histogram1('databases/lanl05_union.db'), set_name='lanl05')
+    #     plot_trace(pdf, *parse_trace_method1('databases/ldns04.db'), set_name='ldns')
+    #     #bar(pdf, *compute_histogram1('databases/ldns04.db'), set_name='ldns')
+    #     plot_trace(pdf, *parse_trace_method1('databases/lri.db'), set_name='lri')
+    #     bar(pdf, *compute_histogram1('databases/lri.db'), set_name='lri')
+    #     plot_trace(pdf, *parse_trace_method1('databases/microsoft99.db'), set_name='microsoft')
+    #     #bar(pdf, *compute_histogram1('databases/microsoft99.db'), set_name='microsoft')
+    #     plot_trace(pdf, *parse_trace_method1('databases/notre_dame07_cpu1.db'), set_name='nd_cpu')
+    #     #bar(pdf, *compute_histogram1('databases/notre_dame07_cpu1.db'), set_name='nd_cpu')
+    #     plot_trace(pdf, *parse_trace_method1('databases/notre_dame07_rood.db'), set_name='nd_rood')
+    #     #bar(pdf, *compute_histogram1('databases/notre_dame07_rood.db'), set_name='nd_rood')
+    #     plot_trace(pdf, *parse_trace_method3('databases/oneping.db'), set_name='oneping')
+    #     bar(pdf, *compute_histogram3('databases/oneping.db'), set_name='oneping')
+    #     plot_trace(pdf, *parse_trace_method2('databases/overnet03.db'), set_name='overnet')
+    #     bar(pdf, *compute_histogram2('databases/overnet03.db'), set_name='overnet')
+    #     plot_trace(pdf, *parse_trace_method1('databases/pnnl07.db'), set_name='pnnl')
+    #     #bar(pdf, *compute_histogram1('databases/pnnl07.db'), set_name='pnnl')
+    #     plot_trace(pdf, *parse_trace_method1('databases/sdsc.db'), set_name='sdsc')
+    #     bar(pdf, *compute_histogram1('databases/sdsc.db'), set_name='sdsc')
+    #     plot_trace(pdf, *parse_trace_method1('databases/teragrid2.db'), set_name='teragrid')
+    #     bar(pdf, *compute_histogram1('databases/teragrid2.db'), set_name='teragrid')
+    #     plot_trace(pdf, *parse_trace_method1('databases/ucb.db'), set_name='ucb')
+    #     bar(pdf, *compute_histogram1('databases/ucb.db'), set_name='ucb')
+    #     # In websites_02, the 0 epoch = 26/09/2001 16:11:10
+    #     # In pyplot, the 0 epoch = 01/01/0001 -1
+    #     #                Days    +1   16:11:10
+    #     websites_epoch = 730753 + 1 + 86400. / (16 * 3600 + 11 * 60 + 10)
+    #     plot_trace(pdf, *parse_trace_method1('databases/websites02.db', epoch=websites_epoch), set_name='websites')
+    #     bar(pdf, *compute_histogram1('databases/websites02.db'), set_name='websites')
