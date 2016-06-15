73429de45f7ca2aabd2577460fced5531718ad16
Sébastien Vaucher <278609+sebva@users.noreply.github.com>
2016-06-15 13:55:39 +0200

    Run the dd benchmark once

--- projects/erasure-tester/container-scripts/benchmarks_impl.py
+++ projects/erasure-tester/container-scripts/benchmarks_impl.py
@@ -185,19 +185,16 @@ class BenchmarksImpl:
         return {}
 
     def bench_dd(self, config=None, redis=None, java=None, nodes_trace=None, block_count=50):
-        write_speed = read_speed = 0
+        filename = self.generate_file_name()
+        out = subprocess.check_output(("dd if=/dev/zero of=%s bs=128kB count=%d" % (filename, block_count))
+                                      .split(' '), stderr=subprocess.STDOUT, universal_newlines=True)
+        match = re.search(r'([0-9.]+) ([a-zA-Z]?B/s)$', out)
+        write_speed = self._convert_to_kb(*match.groups())
 
-        for _ in range(3):
-            filename = self.generate_file_name()
-            out = subprocess.check_output(("dd if=/dev/zero of=%s bs=128kB count=%d" % (filename, block_count))
-                                          .split(' '), stderr=subprocess.STDOUT, universal_newlines=True)
-            match = re.search(r'([0-9.]+) ([a-zA-Z]?B/s)$', out)
-            write_speed = max(self._convert_to_kb(*match.groups()), write_speed)
-
-            out = subprocess.check_output(("dd if=%s of=/dev/null bs=128kB count=%d" % (filename, block_count))
-                                          .split(' '), stderr=subprocess.STDOUT, universal_newlines=True)
-            match = re.search(r'([0-9.]+) ([a-zA-Z]?B/s)$', out)
-            read_speed = max(self._convert_to_kb(*match.groups()), read_speed)
+        out = subprocess.check_output(("dd if=%s of=/dev/null bs=128kB count=%d" % (filename, block_count))
+                                      .split(' '), stderr=subprocess.STDOUT, universal_newlines=True)
+        match = re.search(r'([0-9.]+) ([a-zA-Z]?B/s)$', out)
+        read_speed = self._convert_to_kb(*match.groups())
 
         return {
             'read': read_speed,
