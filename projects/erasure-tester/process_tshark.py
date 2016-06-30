a98e547bb24098fc36f515c802dec74c8424d5e4
Sébastien Vaucher <278609+sebva@users.noreply.github.com>
2016-06-30 13:33:40 +0200

    Script to process the output of tshark

---
+++ projects/erasure-tester/process_tshark.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+import sys
+
+def hour(sec):
+    return float(sec) / 3600
+
+timeres = 15
+
+general_count = 0
+write_count = 0
+read_count = 0
+redis_count = 0
+cluster_count = 0
+exists_count = 0
+
+current_time = 0
+last_print = 0
+
+timestamp = 0.0
+length = 0
+time_bracket = 0
+
+print("sec\thour\tgeneral\twrite\tread\tredis\tcluster\texists")
+
+
+for line in sys.stdin:
+    try:
+        cols = line.split('\t')
+        if len(cols) < 3:
+            continue
+        timestamp = float(cols[0])
+        length = int(cols[1])
+        redis = cols[2]
+        if redis.strip() != '':
+            if redis.startswith('SET'):
+                write_count += length
+            elif redis.startswith('AA'):
+                read_count += length
+            elif redis.startswith('cluster'):
+                cluster_count += length
+            elif redis.startswith('EXISTS'):
+                exists_count += length
+            else:
+                redis_count += length
+        else:
+            general_count += length
+        time_bracket = int(timestamp // timeres) * timeres
+        if time_bracket > current_time:
+            for i in range(last_print + timeres, time_bracket - timeres, timeres):
+                print("{}\t{}\t0\t0\t0\t0\t0\t0".format(i, hour(i)))
+                last_print = i
+                current_time = i + timeres
+        
+            print("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}".format(current_time, hour(current_time), general_count, write_count, read_count, redis_count, cluster_count, exists_count))
+            last_print = current_time
+            current_time = time_bracket
+            general_count = 0
+            write_count = 0
+            read_count = 0
+            redis_count = 0
+            cluster_count = 0
+            exists_count = 0
+    except:
+        pass
+
