cb3dc5a777fbf1260acc831666cdc292a9bda1fb
Sébastien Vaucher <278609+sebva@users.noreply.github.com>
2016-06-30 17:14:59 +0200

    Capture the whole frames

--- projects/erasure-tester/redis-cluster/start_with_capture.sh
+++ projects/erasure-tester/redis-cluster/start_with_capture.sh
@@ -7,5 +7,5 @@ id=$(cut -c -8 /etc/hostname)
 /usr/local/bin/docker-entrypoint.sh "$@" &
 redis_pid=$!
 filename="/capture/rediscapture_$(date +%s)_${id}.pcapng"
-gosu 0:0 dumpcap -q -f "tcp port 6379" -i any -s 64 -w ${filename} &
+gosu 0:0 dumpcap -q -f "tcp port 6379" -i any -w ${filename} &
 wait ${redis_pid}
